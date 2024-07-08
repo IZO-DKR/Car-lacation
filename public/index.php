a089f1a04d8f9e17491b5c6c38e7da09febde6d8
IZO.DKR <issakonte.dev@gmail.com>
2024-07-08 10:01:45 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+
+<!-- Afficher la méthode display -->
+
+<?php
+
+require_once'../src/Entity/Test.php';
+
+use App\Entity\Test;
+
+
+$test = new Test();
+
+echo $test->display();
---
+++ src/Entity/Test.php
@@ -0,0 +1,17 @@
+<!-- creer une class Test -->
+<!-- methode public display-->
+<!-- return Hello, world-->
+
+
+<?php
+
+namespace App\Entity;
+
+class Test
+{
+
+    public function display()
+    {
+        return 'Hello, world';
+    }
+}
